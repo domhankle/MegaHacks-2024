314003ddf86a8948fa542bb5b446a2261305d59c
Victor Londono <vlondon2@gmu.edu>
2024-01-27 12:45:53 -0500

    setup models and hardcoded getplan

--- backend/plan/models.py
+++ backend/plan/models.py
@@ -2,3 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    sets = models.PositiveIntegerField()
+    reps = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class ExercisePlan(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.PositiveIntegerField()
+    exercises = models.ManyToManyField(Exercise)
+
+    def __str__(self):
+        return self.name
