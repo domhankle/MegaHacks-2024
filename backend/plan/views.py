c677b1648a0c5c8532eb7d497d8e91e1d623751a
Victor Londono <vlondon2@gmu.edu>
2024-01-27 12:09:36 -0500

    Sample get plan

--- backend/plan/views.py
+++ backend/plan/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 
 def getPlan(request):
-    return HttpResponse("You are trying to GET a plan")
+    return JsonResponse({"Exercises":["Push-ups", "Sit-ups", "Pull-ups"]})
+
+
+
