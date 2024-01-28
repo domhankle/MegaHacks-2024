6fb663ab00f6d0e197546eb2db88219a22913327
Dom Hankle <domhankle03@gmail.com>
2024-01-27 20:48:55 -0500

    I HATE THIS SHIT

--- frontend/workout-app/src/app/workout/add-workout-plan/add-workout-plan.component.ts
+++ frontend/workout-app/src/app/workout/add-workout-plan/add-workout-plan.component.ts
@@ -25,6 +25,12 @@ export class AddWorkoutPlanComponent {
         this._workoutService.createWorkoutPlan(result).subscribe(
           (result) => {
             console.log('Successfully POSTed the Workout Plan: ', result);
+            this._workoutService.getWorkoutPlans().subscribe((result)=>{
+              console.log("Successful GET for all the plans: ", result)
+            },
+            (error) => {
+              console.log("Failed to GET all the plans!", error);
+            })
           },
           (error) => {
             console.log('Failed to POST the Workout Plan: ', error);
